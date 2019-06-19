d3a984cbf503da03fd5b0f7a7b68de6a305630ba
LebedevMikhailKIT <47885536+LebedevMikhailKIT@users.noreply.github.com>
2019-06-19 20:29:18 +0300

    Commit

---
+++ AppCSharp/AppCSharp/Forms/GameForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AppCSharp
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectionLabel = new System.Windows.Forms.Label();
+            this.GameButton11 = new System.Windows.Forms.Button();
+            this.GameButton12 = new System.Windows.Forms.Button();
+            this.GameButton13 = new System.Windows.Forms.Button();
+            this.GameButton23 = new System.Windows.Forms.Button();
+            this.GameButton22 = new System.Windows.Forms.Button();
+            this.GameButton21 = new System.Windows.Forms.Button();
+            this.GameButton32 = new System.Windows.Forms.Button();
+            this.GameButton31 = new System.Windows.Forms.Button();
+            this.GameButton33 = new System.Windows.Forms.Button();
+            this.GameStatusLabel = new System.Windows.Forms.Label();
+            this.ConnectionStaus = new System.Windows.Forms.Label();
+            this.GameStatus = new System.Windows.Forms.Label();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.YourNumber = new System.Windows.Forms.Label();
+            this.Number = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnectionLabel
+            // 
+            this.ConnectionLabel.AutoSize = true;
+            this.ConnectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConnectionLabel.Location = new System.Drawing.Point(377, 43);
+            this.ConnectionLabel.Name = "ConnectionLabel";
+            this.ConnectionLabel.Size = new System.Drawing.Size(119, 22);
+            this.ConnectionLabel.TabIndex = 9;
+            this.ConnectionLabel.Text = "Соединение:";
+            // 
+            // GameButton11
+            // 
+            this.GameButton11.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton11.Location = new System.Drawing.Point(12, 13);
+            this.GameButton11.Name = "GameButton11";
+            this.GameButton11.Size = new System.Drawing.Size(111, 96);
+            this.GameButton11.TabIndex = 11;
+            this.GameButton11.UseVisualStyleBackColor = true;
+            this.GameButton11.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton12
+            // 
+            this.GameButton12.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton12.Location = new System.Drawing.Point(129, 13);
+            this.GameButton12.Name = "GameButton12";
+            this.GameButton12.Size = new System.Drawing.Size(111, 96);
+            this.GameButton12.TabIndex = 12;
+            this.GameButton12.UseVisualStyleBackColor = true;
+            this.GameButton12.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton13
+            // 
+            this.GameButton13.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton13.Location = new System.Drawing.Point(246, 13);
+            this.GameButton13.Name = "GameButton13";
+            this.GameButton13.Size = new System.Drawing.Size(111, 96);
+            this.GameButton13.TabIndex = 13;
+            this.GameButton13.UseVisualStyleBackColor = true;
+            this.GameButton13.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton23
+            // 
+            this.GameButton23.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton23.Location = new System.Drawing.Point(246, 116);
+            this.GameButton23.Name = "GameButton23";
+            this.GameButton23.Size = new System.Drawing.Size(111, 96);
+            this.GameButton23.TabIndex = 16;
+            this.GameButton23.UseVisualStyleBackColor = true;
+            this.GameButton23.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton22
+            // 
+            this.GameButton22.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton22.Location = new System.Drawing.Point(129, 115);
+            this.GameButton22.Name = "GameButton22";
+            this.GameButton22.Size = new System.Drawing.Size(111, 96);
+            this.GameButton22.TabIndex = 15;
+            this.GameButton22.UseVisualStyleBackColor = true;
+            this.GameButton22.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton21
+            // 
+            this.GameButton21.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton21.Location = new System.Drawing.Point(12, 115);
+            this.GameButton21.Name = "GameButton21";
+            this.GameButton21.Size = new System.Drawing.Size(111, 96);
+            this.GameButton21.TabIndex = 14;
+            this.GameButton21.UseVisualStyleBackColor = true;
+            this.GameButton21.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton32
+            // 
+            this.GameButton32.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton32.Location = new System.Drawing.Point(129, 217);
+            this.GameButton32.Name = "GameButton32";
+            this.GameButton32.Size = new System.Drawing.Size(111, 96);
+            this.GameButton32.TabIndex = 18;
+            this.GameButton32.UseVisualStyleBackColor = true;
+            this.GameButton32.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton31
+            // 
+            this.GameButton31.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton31.Location = new System.Drawing.Point(12, 217);
+            this.GameButton31.Name = "GameButton31";
+            this.GameButton31.Size = new System.Drawing.Size(111, 96);
+            this.GameButton31.TabIndex = 17;
+            this.GameButton31.UseVisualStyleBackColor = true;
+            this.GameButton31.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameButton33
+            // 
+            this.GameButton33.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameButton33.Location = new System.Drawing.Point(246, 218);
+            this.GameButton33.Name = "GameButton33";
+            this.GameButton33.Size = new System.Drawing.Size(111, 96);
+            this.GameButton33.TabIndex = 19;
+            this.GameButton33.UseVisualStyleBackColor = true;
+            this.GameButton33.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // GameStatusLabel
+            // 
+            this.GameStatusLabel.AutoSize = true;
+            this.GameStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameStatusLabel.Location = new System.Drawing.Point(379, 101);
+            this.GameStatusLabel.Name = "GameStatusLabel";
+            this.GameStatusLabel.Size = new System.Drawing.Size(117, 22);
+            this.GameStatusLabel.TabIndex = 20;
+            this.GameStatusLabel.Text = "Статус игры:";
+            // 
+            // ConnectionStaus
+            // 
+            this.ConnectionStaus.AutoSize = true;
+            this.ConnectionStaus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConnectionStaus.Location = new System.Drawing.Point(502, 43);
+            this.ConnectionStaus.Name = "ConnectionStaus";
+            this.ConnectionStaus.Size = new System.Drawing.Size(119, 22);
+            this.ConnectionStaus.TabIndex = 21;
+            this.ConnectionStaus.Text = "Соединение:";
+            // 
+            // GameStatus
+            // 
+            this.GameStatus.AutoSize = true;
+            this.GameStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GameStatus.Location = new System.Drawing.Point(504, 101);
+            this.GameStatus.Name = "GameStatus";
+            this.GameStatus.Size = new System.Drawing.Size(117, 22);
+            this.GameStatus.TabIndex = 22;
+            this.GameStatus.Text = "Статус игры:";
+            // 
+            // NewGame
+            // 
+            this.NewGame.Location = new System.Drawing.Point(397, 252);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(211, 23);
+            this.NewGame.TabIndex = 23;
+            this.NewGame.Text = "Новая Игра";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // YourNumber
+            // 
+            this.YourNumber.AutoSize = true;
+            this.YourNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YourNumber.Location = new System.Drawing.Point(379, 156);
+            this.YourNumber.Name = "YourNumber";
+            this.YourNumber.Size = new System.Drawing.Size(149, 22);
+            this.YourNumber.TabIndex = 24;
+            this.YourNumber.Text = "Вы игрок номер:";
+            // 
+            // Number
+            // 
+            this.Number.AutoSize = true;
+            this.Number.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Number.Location = new System.Drawing.Point(534, 156);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(16, 22);
+            this.Number.TabIndex = 25;
+            this.Number.Text = "-";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 326);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.YourNumber);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.GameStatus);
+            this.Controls.Add(this.ConnectionStaus);
+            this.Controls.Add(this.GameStatusLabel);
+            this.Controls.Add(this.GameButton33);
+            this.Controls.Add(this.GameButton32);
+            this.Controls.Add(this.GameButton31);
+            this.Controls.Add(this.GameButton23);
+            this.Controls.Add(this.GameButton22);
+            this.Controls.Add(this.GameButton21);
+            this.Controls.Add(this.GameButton13);
+            this.Controls.Add(this.GameButton12);
+            this.Controls.Add(this.GameButton11);
+            this.Controls.Add(this.ConnectionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GameForm";
+            this.Text = "Крестики-нолики";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label ConnectionLabel;
+        private System.Windows.Forms.Button GameButton11;
+        private System.Windows.Forms.Button GameButton12;
+        private System.Windows.Forms.Button GameButton13;
+        private System.Windows.Forms.Button GameButton23;
+        private System.Windows.Forms.Button GameButton22;
+        private System.Windows.Forms.Button GameButton21;
+        private System.Windows.Forms.Button GameButton32;
+        private System.Windows.Forms.Button GameButton31;
+        private System.Windows.Forms.Button GameButton33;
+        private System.Windows.Forms.Label GameStatusLabel;
+        private System.Windows.Forms.Label ConnectionStaus;
+        private System.Windows.Forms.Label GameStatus;
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Label YourNumber;
+        private System.Windows.Forms.Label Number;
+    }
+}
+
